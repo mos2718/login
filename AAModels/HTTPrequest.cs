1c6d574b070d901b8bcb301aec980bb57195cca4
Jack Hsiao <chhsiao@gms.tcu.edu.tw>
2023-12-24 08:50:15 +0800

    Add files via upload

---
+++ AAModels/HTTPrequest.cs
@@ -0,0 +1,169 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FHIR_LMS_WEBAPI_CORE.Models
+{
+    public class HTTPrequest
+    {
+        public JObject getResource(string fhirUrl, string ResourceName, string Parameter, string token, Func<JObject, JObject, string, JObject> CallbackFunction, JObject loginData)
+        {
+            dynamic errmsg = new JObject();
+
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                var requestHttp = (HttpWebRequest)WebRequest.Create(fhirUrl + ResourceName + Parameter);
+                requestHttp.ContentType = "application/json";
+                requestHttp.Method = "GET";
+                requestHttp.Headers["Authorization"] = token;
+                var response = (HttpWebResponse)requestHttp.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                        JObject resultJson = JObject.Parse(result);
+
+                        if (resultJson["resourceType"].ToString() == "Bundle")
+                        {
+                            string type = resultJson["type"].ToString();
+                            if (type == "searchset" && (int)resultJson["total"] <= 0 && ResourceName!="Appointment")
+                            {
+                                errmsg.total = 0;
+                                errmsg.message = ResourceName + " does not exist.";
+                                errmsg.code = 404;
+                                return errmsg;
+                            }
+                        }
+                        else if (resultJson["resourceType"].ToString() == "OperationOutcome")
+                        {
+                            foreach (JObject issue in resultJson["issue"])
+                            {
+                                errmsg.message += issue["diagnostics"]+ "\n";
+                                errmsg.code = 404;
+                            }
+                                return errmsg;
+                        }
+
+                        if (CallbackFunction == null)
+                        {
+                            return resultJson;
+                        }
+                        else
+                        {
+                            return CallbackFunction(resultJson, loginData, token);
+                        }
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                errmsg.message = loginData["errmsg"] + "\n" + e.Message;
+                return errmsg;
+            }
+            errmsg.message = loginData["errmsg"];
+            return errmsg;
+        }
+
+        public JObject postResource(string fhirUrl, string ResourceName, JObject body, string token, Func<JObject, JObject, string, JObject> CallbackFunction, JObject loginData)
+        {
+            dynamic errmsg = new JObject();
+
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                var requestHttp = (HttpWebRequest)WebRequest.Create(fhirUrl + ResourceName);
+                requestHttp.ContentType = "application/json";
+                requestHttp.Method = "POST";
+                requestHttp.Headers["Authorization"] = token;
+                string postBody = body.ToString();
+                byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(postBody);
+                using (Stream reqStream = requestHttp.GetRequestStream())
+                {
+                    reqStream.Write(byteArray, 0, byteArray.Length);
+                }
+
+                var response = (HttpWebResponse)requestHttp.GetResponse();
+                if (response.StatusCode == HttpStatusCode.Created)
+                {
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                        JObject resultJson = JObject.Parse(result);
+
+                        if (CallbackFunction == null)
+                        {
+                            return resultJson;
+                        }
+                        else
+                        {
+                            return CallbackFunction(resultJson, loginData, token);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                errmsg.message = loginData["errmsg"] + "\n" + e.Message;
+                return errmsg;
+            }
+            errmsg.message = loginData["errmsg"];
+            return errmsg;
+        }
+
+        public JObject putResource(string fhirUrl, string ResourceName, JObject body, string token, Func<JObject, JObject, string, JObject> CallbackFunction, JObject loginData)
+        {
+            dynamic errmsg = new JObject();
+
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                var requestHttp = (HttpWebRequest)WebRequest.Create(fhirUrl + ResourceName);
+                requestHttp.ContentType = "application/json";
+                requestHttp.Method = "PUT";
+                requestHttp.Headers["Authorization"] = token;
+                string putBody = body.ToString();
+                byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(putBody);
+                using (Stream reqStream = requestHttp.GetRequestStream())
+                {
+                    reqStream.Write(byteArray, 0, byteArray.Length);
+                }
+
+                var response = (HttpWebResponse)requestHttp.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Created)
+                {
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                        JObject resultJson = JObject.Parse(result);
+                        if (CallbackFunction != null)
+                        {
+                            JObject callbackResult = CallbackFunction(resultJson, loginData, token);
+                            return callbackResult;
+                        }
+                        return resultJson;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                errmsg.message = loginData["errmsg"] + "\n" + e.Message;
+                return errmsg;
+            }
+            errmsg.message = loginData["errmsg"];
+            return errmsg;
+        }
+    }
+}
---
+++ AAModels/LoginModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace FHIR_LMS_WEBAPI_CORE.Models
+{
+    public class LoginModel
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string fhirUrl;
+        HTTPrequest HTTPrequest;
+        public LoginModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            fhirUrl = _configuration.GetValue<string>("TZFHIR_Url");
+            HTTPrequest = new HTTPrequest();
+        }
+
+        public JObject Check(JObject personUser, JObject loginData, string token)
+        {
+            dynamic result = new JObject();
+            if (personUser["resourceType"].ToString() == "Bundle" && ((JArray)personUser["entry"]).Count == 1)
+            {
+                personUser = (JObject)personUser["entry"][0]["resource"];
+            }
+
+            //Verify email and password
+            if (personUser["identifier"].HasValues && personUser["identifier"][0] != null && personUser["identifier"][1] != null)
+            {
+                if (personUser["identifier"][0]["system"].ToString() == "UserID" && personUser["identifier"][1]["system"].ToString() == "Password")
+                {
+                    string a = (string)personUser["identifier"][0]["value"];
+                    string b = (string)loginData["person"]["email"];
+                    if (a == b)
+                    {
+                        a = (string)personUser["identifier"][1]["value"];
+                        b = (string)loginData["person"]["password"];
+                        if (a == b)
+                        {
+                            //Email and password verified
+                            loginData["person"]["id"] = personUser["id"] != null ? personUser["id"] : "";
+                            loginData["person"]["name"] = personUser["name"] != null && personUser["name"][0]["text"] != null ? personUser["name"][0]["text"] : "";
+                            loginData["person"]["email"] = personUser["identifier"] != null && personUser["identifier"][0] != null ? personUser["identifier"][0]["value"] : "";
+
+                            result.roles = GetRoles(personUser, loginData);
+
+                            result.code = 200;
+                            return result;
+                        }
+                    }
+                }
+            }
+            result.code = 401;
+            return result;
+        }
+
+        private JArray GetRoles(JObject personUser, JObject loginData)
+        {
+            //Get Roles
+            JArray roles = new JArray();
+
+            foreach (JObject role in (JArray)personUser["link"])
+            {
+                string roleID = role["target"]["reference"].ToString().Split('/')[1];
+                JObject userRole = JObject.Parse(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Assets/JSON/Role.json")));
+                loginData["errmsg"] = "Error fetching user roles.";
+
+                //Get Patient Roles
+                if (role["target"]["reference"].ToString().StartsWith("Patient/"))
+                {
+                    JObject res = HTTPrequest.getResource(fhirUrl, "Patient", "/" + roleID, "", null, loginData);
+                    loginData["person"]["name"] = loginData["person"]["name"].ToString() == "" ? res["name"][0]["text"] : loginData["person"]["name"].ToString();
+                    userRole["roleName"] = res["resourceType"];
+                    userRole["roleID"] = res["id"];
+                    userRole["practID"] = "";
+                    userRole["organizationID"] = res["managingOrganization"]["reference"].ToString().Split('/')[1];
+                    userRole["organizationName"] = res["managingOrganization"]["display"] ?? res["managingOrganization"]["reference"];
+                    userRole["status"] = res["active"] != null ? "active" : "";
+
+                    roles.Add(userRole);
+                }
+                //Get Practitioner Roles
+                else if (role["target"]["reference"].ToString().StartsWith("Practitioner/"))
+                {
+                    string practitionerID = role["target"]["reference"].ToString().Substring(13);
+
+                    string param = "?practitioner=" + practitionerID;
+                    JObject res = HTTPrequest.getResource(fhirUrl, "PractitionerRole", param, null, null, loginData);
+
+                    JArray pracRoles = (JArray)res["entry"];
+
+                    loginData["person"]["name"] = loginData["person"]["name"].ToString() == "" ? res["name"][0]["text"] : loginData["person"]["name"].ToString();
+
+                    foreach (JObject pracRole in pracRoles)
+                    {
+                        userRole["roleName"] = pracRole["resource"]["resourceType"];
+                        userRole["roleID"] = pracRole["resource"]["id"];
+                        userRole["practID"] = pracRole["resource"]["practitioner"]["reference"].ToString().Split('/')[1];
+                        userRole["organizationID"] = pracRole["resource"]["organization"]["reference"].ToString().Split('/')[1];
+                        userRole["organizationName"] = pracRole["resource"]["organization"]["display"];
+                        userRole["status"] = (bool)pracRole["resource"]["active"] ? "active" : "";
+                        if (pracRole["resource"]["code"]!=null)
+                        {
+                            foreach (JObject coding in pracRole["resource"]["code"][0]["coding"])
+                            {
+                                userRole["roleCode"].ToList().Add(coding["code"]);
+                            }
+                        }
+
+
+                        roles.Add(userRole);
+                    }
+                }
+            }
+
+            return roles;
+        }
+
+    }
+}
